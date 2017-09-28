c664548ac4169971d4d7e54e584288521877dcfe
Paulo Romeira <paulo@pauloromeira.com>
2017-09-28 17:00:38 -0300

    Pequenas modficações

--- presentation/branches/index.js
+++ presentation/branches/index.js
@@ -10,7 +10,14 @@ var master = gitGraph1.branch("master");
 master.commit().commit().commit(); 
 var develop = master.branch("develop"); 
 develop.commit();
-master.commit().commit().commit(); 
+develop.commit({
+  dotColor: "white",
+  dotSize: 10,
+  dotStrokeWidth: 10,
+  tag: "feature/LEGYS-500",
+});
+master.commit().commit(); 
+master.commit({tag: "master"});
 
 var gitGraph2 = new GitGraph({
   template: "metro",
